3d6c61fd88518a1f84b1ab3fcb9cf17550af563c
Jason Grimes <jason@grimesit.com>
2024-01-03 10:02:16 -0500

    Fix layout jiggling on ios

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="relative min-h-svh">
+    <div className="relative min-h-screen">
       <Header />
       <main className=" max-w-[100vw] pb-24">
         <FretboardEditor />
