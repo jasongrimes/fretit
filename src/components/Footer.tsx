81a5c88868b20245743e0afd820637ff77e1ffb5
Jason Grimes <jason@grimesit.com>
2024-01-09 17:02:59 -0500

    Credit Grimes IT

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,10 @@ export default function Footer() {
     <footer className="footer footer-center absolute bottom-0 bg-base-300 p-4 text-base-content">
       <aside>
         <p className="leading-loose">
-          Made with ❤️ by{" "}
-          <a href="https://jasonkgrimes.com" className="link">
-            Jason Grimes
-          </a>
+          Made with ❤️ by {" "}
+          <a href="https://grimesit.com" className="link">
+            Grimes IT
+          </a> 
           . <span className="whitespace-nowrap">Copyright © 2024</span>
         </p>
       </aside>
