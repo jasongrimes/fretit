5b63daa4d1c3ef4dc7d1d9017ea238fe26ad7554
Jason Grimes <jason@grimesit.com>
2024-01-10 05:19:42 -0500

    Pull out data into separate files

---
+++ src/data/instruments.tsx
@@ -0,0 +1,10 @@
+import { Instrument } from "../services/fretboard";
+
+export const INSTRUMENTS: Record<string, Instrument> = {
+  Guitar: {
+    name: "Guitar",
+    tuning: [64, 59, 55, 50, 45, 40],
+    fretMarkers: [3, 5, 7, 9, 12, 15, 17, 19, 21, 24],
+    doubleFretMarkers: [12, 24],
+  },
+};
