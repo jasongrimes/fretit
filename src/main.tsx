e2e6c0c8dff3d898c2c58a6dd85db93430aea509
Jason Grimes <jason@grimesit.com>
2024-01-16 06:30:11 -0500

    Install Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://e1d04366240df9b94307e86e6ca1463c@o4506580425244672.ingest.sentry.io/4506580432781312",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/fretit\.io/],
+    }),
+    new Sentry.Replay({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
