1dc48c8af0e5511a62856794a76345540df2f048
Jason Grimes <jason@grimesit.com>
2024-01-17 08:04:11 -0500

    Disable Sentry in development

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,12 @@ import App from './App.tsx'
 import './index.css'
 import * as Sentry from "@sentry/react";
 
+// https://docs.sentry.io/platforms/javascript/configuration/
 Sentry.init({
   dsn: "https://e1d04366240df9b94307e86e6ca1463c@o4506580425244672.ingest.sentry.io/4506580432781312",
+
+  enabled: import.meta.env.PROD,
+
   integrations: [
     new Sentry.BrowserTracing({
       // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
