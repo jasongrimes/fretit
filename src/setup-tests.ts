2000d1744169771698ba51f551ba45f016b39af1
Jason Grimes <jason@grimesit.com>
2024-01-20 07:39:53 -0500

    Mock window.scrollTo in tests

--- src/setup-tests.ts
+++ src/setup-tests.ts
@@ -2,6 +2,8 @@ import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+window.scrollTo = () => {};
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
