5db36ad15795cd6c75a94e5c92d088ce74a28e48
Jason Grimes <jason@grimesit.com>
2024-01-21 15:57:51 -0500

    Add debug comments

--- src/utils/sound-player.ts
+++ src/utils/sound-player.ts
@@ -25,7 +25,16 @@ export default class SoundPlayer {
     }
     const midi: number = this.tuning[stringNum - 1] + fretNum;
 
+    // console.log(this.audioContext.state);
     void this.audioContext.resume().then(() => {
+      // FIXME: On iOS Safari, after switching tabs for a bit, sound stops working.
+      // We get this far and this.audioContext.state is "running",
+      // and this.player.loaded() is true,
+      // but this.audioContext.currentTime does not update,
+      // which may suggest a way to identify this broken state.
+      // We can't just compare it to the last audioContext.currentTime, however,
+      // because this is called multiple times in a row when strumming a chord.
+      // See https://github.com/jasongrimes/fretit/issues/2
       this.stringMuters[stringNum - 1] = this.player.start({
         note: midi,
         velocity: 127,
