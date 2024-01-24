8ec89c08186deef5b060de039ccf1be1dab1fe39
Jason Grimes <jason@grimesit.com>
2024-01-24 07:04:28 -0500

    Rework layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,12 @@
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import { MaximizedContext } from "@/providers/maximized";
 import { Outlet } from "@tanstack/react-router";
-import { useState } from "react";
 
 export default function Layout() {
-  const [maximized, setMaximized] = useState(false);
-
   return (
-    <div className="relative min-h-screen">
-      {!maximized && <Header />}
-      <main className=" max-w-[100vw] pb-24">
-        <MaximizedContext.Provider value={{ maximized, setMaximized }}>
-          <Outlet />
-        </MaximizedContext.Provider>
-      </main>
+    <div className="min-h-screen flex flex-col">
+      <div className="grow">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
