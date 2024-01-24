8f158bddc1348b07afbfd892294152d6a841e973
Jason Grimes <jason@grimesit.com>
2024-01-24 07:08:31 -0500

    Move maximized context back into state

--- src/providers/maximized.tsx
+++
@@ -1,8 +0,0 @@
-import { Dispatch, SetStateAction, createContext } from "react";
-
-interface MaximizedContextInterface {
-  maximized: boolean;
-  setMaximized: Dispatch<SetStateAction<boolean>>;
-}
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export const MaximizedContext = createContext<MaximizedContextInterface>({ maximized: false, setMaximized: () => {} });
